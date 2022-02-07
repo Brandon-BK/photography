8c5b403e671247d54677a3a8bb883e2c561d09aa
Brandon <kisibubrandon5@gmail.com>
2022-02-07 15:36:05 +0200

    portfolio done

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Box from '@mui/material/Box';
 import Banner from '../components/Banner';
+import Portfolio from '../components/Portfolio';
 
 export default function Home() {
   return (
     <Box>
       <Banner />
+      <Portfolio />
     </Box>
   )
 }
